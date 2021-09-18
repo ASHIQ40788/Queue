f99869cf2d42c70cd04bba146d1839334632d8dd
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-18 20:42:42 +0530

    [ASHIQ] added : Enqueue and Dequeue file

---
+++ LinkedListQueue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class LinkedListQueue
+    {
+        Node head;
+        public void Enqueue(int item)
+        {
+            Node new_node = new Node(item);
+            if (this.head == null)
+            {
+                this.head = new_node;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = new_node;
+            }
+            Console.WriteLine("Inserted int list" + new_node.data);
+        }
+        public void Dequeue()
+        {
+            //If queue is empty,return NULL.
+            if (this.head == null)
+            {
+                Console.WriteLine("the Queue is empty");
+                return;
+            }
+            //store previous head and move head pointer one step ahead to delete the current element
+            Node temp = this.head;
+            this.head = this.head.next;
+            Console.WriteLine("Item deleted is {0}", temp.data);
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data);
+                }
+            }
+        }
+    }
+}               
+           
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListQueue queue = new LinkedListQueue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+
+            Queue<int> queue1 = new Queue<int>();
+            queue1.Enqueue(56);
+            queue1.Enqueue(30);
+            queue1.Enqueue(70);
+
+            foreach(int num in queue1)
+            {
+                Console.WriteLine(num);
+            }
+
+            queue1.Dequeue();
+            Console.WriteLine("After delete");
+            foreach (int num in queue1)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
